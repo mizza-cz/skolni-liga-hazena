1f2071fec30e296d6cb4c45499eaf08016c1da49
mizza-cz <mizza.cz666@gmail.com>
2025-08-06 14:04:26 +0200

    add fixes

--- src/components/ambassadors-section/ambassadors-section.js
+++ src/components/ambassadors-section/ambassadors-section.js
@@ -8,16 +8,13 @@ new Swiper(".ambassadorsSwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    type: "bullets",
+    bulletClass: "swiper-pagination-bullet",
+    bulletActiveClass: "swiper-pagination-bullet-active",
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 5,
-    },
+    640: { slidesPerView: 2 },
+    768: { slidesPerView: 3 },
+    1024: { slidesPerView: 5 },
   },
 });
